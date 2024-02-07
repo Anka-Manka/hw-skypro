61b805db97ae11263e052e881876a07da6ab6d51
Anka-Manka <i@annryzh.ru>
2024-02-07 21:00:04 +0300

    hw-4

---
+++ hw-4.js
@@ -0,0 +1,20 @@
+// Task 1;
+
+for (let i = 0; i < 2; i++) {
+    console.log('Привет');;
+}
+
+
+
+// Task 2;
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+// Task 3;
+
+for (let i = 7; i <= 22; i++) {
+    console.log(i);
+}
